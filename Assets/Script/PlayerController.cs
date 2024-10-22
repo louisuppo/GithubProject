f3a5616ef0f27c4309b36190e712470864a2d3d7
Louis SUPPO <louis.suppo@artfx.fr>
2024-10-22 14:59:38 +0200

    Basic Tank Move

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,17 +7,28 @@ public class PLayerController : MonoBehaviour
 {
 
     public float speed = 10f;
+    public float rotationSpeed = 10f;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.up * speed * Time.deltaTime;
         }
 
     }
